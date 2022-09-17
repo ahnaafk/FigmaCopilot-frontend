26bc7d1445ad3ba9af27968c160ced834b2a649f
Ahnaaf Khan <ahnaafk@gmail.com>
2022-09-17 09:03:02 -0400

    Added comments

--- code.ts
+++ code.ts
@@ -12,7 +12,11 @@ const imageTest = figma.currentPage.selection[0]
 async function getImageHash() {
 
   const bytes = await imageTest.exportAsync() //turns the selected image into bytes
+  //send the bytes somewhere
+
+  //take in bytes from somewhere else
   const image = figma.createImage(bytes) //turns bytes into image on figma
+
   const frame = figma.createFrame() //creates frame to hold the image
   
   //places and sizes the frame. 
@@ -31,6 +35,8 @@ async function getImageHash() {
 // posted message.
 figma.showUI(__html__, {themeColors: true});
 
+
+//msg from html gets passed here to call main func
 figma.ui.onmessage = async (msg) => {
   if (msg.type === 'prompt') {
     console.log("Submitted")
